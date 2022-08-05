79a32fbaef1c5352bca3b3d6fe782853c727cf9a
mithila-panagoda <mithilapanagoda@gmail.com>
2022-08-05 04:27:47 -0700

    TRAC-5629-clean and formated code

--- src/app/admin/browse-marketplace/browse-marketplace.component.ts
+++ src/app/admin/browse-marketplace/browse-marketplace.component.ts
@@ -9,35 +9,8 @@ import { ApiServicesService } from 'src/app/services/api-services/api-services.s
   styleUrls: ['./browse-marketplace.component.css'],
 })
 export class BrowseMarketplaceComponent implements OnInit {
-  private endorse:Endorse;
-  activites1:any[]
-  activities: any[] = [
-    {
-      name: 'JOHN ARC',
-      date: '2022/7/8',
-      pk: 'khggfjhgri7687838938yr8',
-    },
-    {
-      name: 'JOHN ARC',
-      date: '2022/7/8',
-      pk: 'khggfjhgri7687838938yr8',
-    },
-    {
-      name: 'JOHN ARC',
-      date: '2022/7/8',
-      pk: 'khggfjhgri7687838938yr8',
-    },
-    {
-      name: 'JOHN ARC',
-      date: '2022/7/8',
-      pk: 'khggfjhgri7687838938yr8',
-    },
-    {
-      name: 'JOHN ARC',
-      date: '2022/7/8',
-      pk: 'khggfjhgri7687838938yr8',
-    },
-  ];
+  pendingEndorsments:any[]
+  
 
   notifications: any[] = [
     {
@@ -61,9 +34,7 @@ export class BrowseMarketplaceComponent implements OnInit {
   ngOnInit(): void {
     let status="Pending"
     this.service.getEndorsementByStatus(status).subscribe((data:any)=>{
-        console.log("data: ",data.Response)
-        this.activites1=data.Response;
-        console.log("array:",this.activites1)
+        this.pendingEndorsments=data.Response;
     });
   }
 }

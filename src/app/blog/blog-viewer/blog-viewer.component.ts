4ed35c3cd59e45a3cc40a376a323223fb3044af7
Keleigh123 <luciaberenger@gmail.com>
2022-09-02 09:26:15 +0530

    TRAC-5953-Revamping activity and stories

--- src/app/blog/blog-viewer/blog-viewer.component.ts
+++ src/app/blog/blog-viewer/blog-viewer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { ApiServicesService } from 'src/app/services/api-services/api-services.service';
 
@@ -10,19 +11,22 @@ import { ApiServicesService } from 'src/app/services/api-services/api-services.s
 export class BlogViewerComponent implements OnInit {
   data: any;
   story: any;
+  nftstory: any;
 
   constructor(private route:ActivatedRoute,
-    private service:ApiServicesService) { }
+    private service:ApiServicesService,
+    private domSanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
       this.data = JSON.parse(params['data']);
       console.log('data passed :', this.data);
-      this.service.getAllStoryByNFTIdAndBlockchain(this.data.nftidentifier,this.data.blockchain).subscribe((res1:any)=>{
+      this.service.getAllStoryByNFTIdAndBlockchain('7kKKNXTut4jtZBDZNMSMAfY42r2CJum5RBXpwAvpYAD3','solana').subscribe((res1:any)=>{//this.data.nftidentifier,this.data.blockchain
         console.log("story retrieved ",res1)
         console.log("story retrieved ",res1.Response[0].NFTStory)
         
         this.story=res1.Response[0].NFTStory
+        this.nftstory=this.domSanitizer.bypassSecurityTrustHtml(this.story)
       })
   })
 

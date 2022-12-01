b10807226efadeb5f8873219383fd794d6d52c55
Mithila Panagoda <mithilapanagoda@gmail.com>
2022-12-01 15:53:05 +0530

    TRAC-6761 Code clean up

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'src/app/models/mail';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SnackbarServiceService } from 'src/app/services/snackbar-service/snackbar-service.service';
 import { Router } from '@angular/router';
+import { APIConfigENV } from 'src/environments/environment';
 
 @Component({
   selector: 'app-footer',
@@ -13,16 +14,17 @@ import { Router } from '@angular/router';
 export class FooterComponent implements OnInit {
   subscribe: Subscription = new Subscription('');
   controlGroup: FormGroup;
-  private readonly mkURL:string=`http://localhost:4200`
-   readonly mkHome:string=`${this.mkURL}/home`
-   readonly mkETHURL:string=`${this.mkURL}/explore?blockchain=ethereum&filter=all`
-   readonly mkPolygon:string=`${this.mkURL}/explore?blockchain=polygon&filter=all`
-   readonly mkStellar:string=`${this.mkURL}/explore?blockchain=stellar&filter=all`
-   readonly mkSolana:string=`${this.mkURL}/explore?blockchain=solana&filter=all`
-   readonly mkBlog:string=`${this.mkURL}/nft-story`
-   readonly mkFaq:string=`${this.mkURL}/faq`
-   readonly mkContact_us:string=`${this.mkURL}/contact-us`
-   readonly mkDocs:string=`${this.mkURL}/docs`
+  private readonly marketplaceBaseURL = APIConfigENV.marketplaceBaseURL
+  private readonly mkURL:string=this.marketplaceBaseURL
+   readonly mkHome:string=`${this.mkURL}home`
+   readonly mkETHURL:string=`${this.mkURL}explore?blockchain=ethereum&filter=all`
+   readonly mkPolygon:string=`${this.mkURL}explore?blockchain=polygon&filter=all`
+   readonly mkStellar:string=`${this.mkURL}explore?blockchain=stellar&filter=all`
+   readonly mkSolana:string=`${this.mkURL}explore?blockchain=solana&filter=all`
+   readonly mkBlog:string=`${this.mkURL}nft-story`
+   readonly mkFaq:string=`${this.mkURL}faq`
+   readonly mkContact_us:string=`${this.mkURL}contact-us`
+   readonly mkDocs:string=`${this.mkURL}docs`
   constructor(
     private service:ApiServicesService,
     private snackbarService:SnackbarServiceService,

ebc80a1314e0da7de9f1ced0ff02c0aa06d885df
Mithila Panagoda <mithilapanagoda@gmail.com>
2023-03-09 15:54:26 +0530

    TRAC-7662-search-is-not-working-nft-marketplace

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  contractAddressNFTPolygon: "0xc89bac690365d942ba49bb020274440dae54ec11",
-  contractAddressMKPolygon: "0x7D591543D2A09ef03AaB1490083943fC69c352Ec",
+  contractAddressNFTPolygon: "0xE6F59c4A3118C0C00155b00331dF07b40DD85569",
+  contractAddressMKPolygon: "0x86E328AD08b5e66B6AcBF7e03DAC74c7b723f5B7",
   contractAddressNFTEthereum: "0x6f09Ecb9e29bF5B1d2C9c39d126FF32dc089F34C",
   contractAddressMKEthereum: "0x7D591543D2A09ef03AaB1490083943fC69c352Ec",
   fromWallet: "4wtzfvaCWvYxzmdh4ajQoj8ep5AuJtUeV8rW3D5biCT7",

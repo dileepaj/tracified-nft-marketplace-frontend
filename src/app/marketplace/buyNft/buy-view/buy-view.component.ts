5d1a23e808deacc48b3f2e7a250095153f7605fb
Keleigh123 <luciaberenger@gmail.com>
2023-05-19 11:45:18 +0530

    TRAC-8491-Implementing a pipe to catch error when sending request to queue

--- src/app/marketplace/buyNft/buy-view/buy-view.component.ts
+++ src/app/marketplace/buyNft/buy-view/buy-view.component.ts
@@ -252,7 +252,18 @@ export class BuyViewComponent implements OnInit {
     this.queue.ImageBase64=this.NFTList.imagebase64;
     this.queue.NFTIdentifier=this.nftbe.NFTIdentifier;
     this.queue.Version=(this.count).toString();
-    this.service.queueBuys(this.queue).subscribe(res=>{
+    this.lockData()
+  }
+
+  lockData(){
+    this.service.queueBuys(this.queue)
+    .pipe(
+      catchError((error) => {
+        this.buyNFT();
+        return throwError('Something went wrong');
+      })
+    )
+    .subscribe(res=>{
       this.getProcessedData()
     })
   }
@@ -261,14 +272,20 @@ export class BuyViewComponent implements OnInit {
     this.service.getQueueData(this.NFTList.imagebase64,this.nftbe.Blockchain,this.count) 
     .pipe(
       catchError((error) => {
-        this.getProcessedData();
+        this.buyNFT();
         return throwError('Something went wrong');
       })
     )
     .subscribe((data:any)=>{
-      if(data.Status=="PROCESSED"){
-        this.userPK=data.User
-        this.updateBackend(this.userPK);
+      if(data==null){
+          this.buyNFT()
+      }else{
+        if(data.Status=="PROCESSED"){
+          this.userPK=data.User
+          this.updateBackend(this.userPK);
+        }else{
+          this.buyNFT()
+        }
       }
     })
   }

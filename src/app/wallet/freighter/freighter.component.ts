3c4839a2a7dc2f789a8bc73e98d2de18f7c6d5e8
Keleigh123 <luciaberenger@gmail.com>
2023-07-25 18:12:20 +0530

    TRAC-9014-Royalty can have other characters rather than numeric values - NFT marketplace

--- src/app/wallet/freighter/freighter.component.ts
+++ src/app/wallet/freighter/freighter.component.ts
@@ -40,7 +40,7 @@ export class FreighterComponent implements Wallet, OnInit {
   ngOnInit(): void {}
 
   async initWallelt() {
-    if ((window as any).freighterApi.isConnected()) {
+    if (await isConnected()) {
       return;
     } else {
         if (typeof (window as any).freighterApi != 'undefined'){
